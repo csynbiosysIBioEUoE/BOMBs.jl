ae21e4b66edfe698af322a031c5cfb4c996ae28f
DavidGomezC <s1778490@sms.ed.ac.uk>
2021-11-16 09:31:41 +0000

    Fix multiobservable case check

--- src/ModelPDat.jl
+++ src/ModelPDat.jl
@@ -611,7 +611,7 @@ function checkStructPseudoDat(model_def, pseudo_def)
            convert(Bool,sum(occursin.("*", pseudo_def["Obs"]))) || convert(Bool,sum(occursin.("^", pseudo_def["Obs"])))
             nothing
         else
-            if sum(occursin.(model_def["stName"], pseudo_def["Obs"])) == 0
+            if sum([sum(occursin.(model_def["stName"], pseudo_def["Obs"][k])) for k in 1:length(model["Obs"])]) == 0
                 println("-------------------------- Process STOPPED!!! --------------------------")
                 println(string("Sorry, but there is some issue with the contents of the field Obs."))
                 println("It seems that the observable(s) selected do not match any state")
